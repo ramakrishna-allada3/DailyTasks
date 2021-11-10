000b22b5dd90223dea120bd4a90cb757995be1d0
ramakrishna-allada3 <ramakrishna.allada3@gmail.com>
2021-11-10 11:46:08 +0530

    Added day 3 tasks

---
+++ Day 3/Task1.js
@@ -0,0 +1,81 @@
+var json = {
+  "colors": [
+    {
+      "color": "black",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,255,255,1],
+        "hex": "#000"
+      }
+    },
+    {
+      "color": "white",
+      "category": "value",
+      "code": {
+        "rgba": [0,0,0,1],
+        "hex": "#FFF"
+      }
+    },
+    {
+      "color": "red",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,0,0,1],
+        "hex": "#FF0"
+      }
+    },
+    {
+      "color": "blue",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [0,0,255,1],
+        "hex": "#00F"
+      }
+    },
+    {
+      "color": "yellow",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,255,0,1],
+        "hex": "#FF0"
+      }
+    },
+    {
+      "color": "green",
+      "category": "hue",
+      "type": "secondary",
+      "code": {
+        "rgba": [0,255,0,1],
+        "hex": "#0F0"
+      }
+    },
+  ]
+}
+
+// for loop
+console.log("for loop");
+for (let count = 0; count < json.colors.length ; count++) {
+    console.log(json.colors[count]);
+}
+
+// for in
+console.log("for in");
+var index = 0;
+for (index in json.colors) {
+    console.log(json.colors[index]);
+    index++;
+} 
+
+// for of
+console.log("for of");
+for (let color of json.colors) {
+  console.log(color);
+}
+
+// forEach
+console.log("forEach");
+json.colors.forEach(item => console.log(item));
